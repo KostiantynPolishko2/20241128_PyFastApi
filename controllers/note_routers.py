b48ff84bd6e474a8995a36cd4ca9dcad00d327b8
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-12-02 00:21:49 +0200

    3 done Dependency Injection of note_service in APIRouter

--- controllers/note_routers.py
+++ controllers/note_routers.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, status, HTTPException
-from typing import List
+from fastapi import APIRouter, status, HTTPException, Depends
+from typing import List, Annotated
 from entities import Note
-from repositories import note_repository
+from services.note_service import NoteService
 
 router = APIRouter(
     prefix='/api/v1',
@@ -9,30 +9,38 @@ router = APIRouter(
     # responses={status.HTTP_400_BAD_REQUEST: {'description' : 'Bad Request'}}
 )
 
+def get_note_service()->NoteService:
+    return NoteService()
+
+note_service_dependency = Annotated[NoteService, Depends(get_note_service)]
+
+
 @router.get("/", status_code=status.HTTP_200_OK)
-async def get_notes()->List[Note]:
-    return note_repository.get_notes()
+async def get_notes(note_service: note_service_dependency)->List[Note]:
+    return note_service.get_notes()
+
 
 @router.get("/{id}", status_code=status.HTTP_200_OK)
-async def get_note_by_id(id: int)->Note:
-    return note_repository.get_note_by_id(id)
+async def get_note_by_id(id: int, note_service: note_service_dependency)->Note:
+    return note_service.get_note_by_id(id)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def add_note(note: Note):
+async def add_note(note: Note, note_service: note_service_dependency):
     if note == None:
         raise HTTPException(status_code=400, detail='bad request')
 
-    return note_repository.add_note(note)
+    return note_service.add_note(note)
+
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-async def update_note(id: int, note: Note):
-
+async def update_note(id: int, note: Note, note_service: note_service_dependency):
     if note == None:
         raise HTTPException(status_code=400, detail='bad request')
 
-    return note_repository.update_note(id, note)
+    return note_service.update_note(id, note)
+
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK)
-async def delete_note(id: int):
-    return note_repository.delete_note(id)
+async def delete_note(id: int, note_service: note_service_dependency):
+    return note_service.delete_note(id)
---
+++ services/note_service.py
@@ -0,0 +1,53 @@
+from fastapi import HTTPException, status
+from typing import List, Dict
+from entities import Note
+from context import notes
+
+class NoteService:
+    def get_notes(self)-> List[Note] | HTTPException:
+        if len(notes) == 0:
+            raise HTTPException(status_code=404, detail='not found')
+
+        return notes
+
+    def get_note_by_id(self, id: int)->Note | HTTPException:
+        for note in notes:
+            if id == note.id:
+                return note
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Note id {id} not found!')
+
+    def is_title_exist(self, title: str)->HTTPException | None:
+        _title = title.lower()
+
+        for note in notes:
+            if _title == note.title.lower():
+                raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f'note title \"{title}\" is already exist in db')
+
+        return None
+
+    def add_note(self, note: Note)->HTTPException | dict[str:int]:
+        self.is_title_exist(note.title)
+        note.id = notes[len(notes)-1].id+1
+        notes.append(note)
+
+        return {"response": status.HTTP_201_CREATED}
+
+    def update_note(self, id: int, new_note: Note)->HTTPException | dict[str:int]:
+        for note in notes:
+            if id == note.id:
+                note = new_note
+                note.id = id
+                return {"response": status.HTTP_202_ACCEPTED}
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Note id {id} not found!')
+
+    def delete_note(self, id: int)->HTTPException | dict[str:int]:
+        if len(notes)==0:
+            raise HTTPException(status_code=404, detail='not found')
+        for note in notes:
+            if id == note.id:
+                notes.remove(note)
+                return {"response": status.HTTP_205_RESET_CONTENT}
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Note id {id} not found!')
